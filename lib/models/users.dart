023b8fd7d30b643118dc77b7e21d88e468bb6a62
Nahom Fufa <nahomn7@gmail.com>
2023-03-26 22:22:17 +0300

    models first draft

---
+++ lib/models/users.dart
@@ -0,0 +1,53 @@
+// import 'package:flutter/foundation.dart';
+
+class User {
+  final String id;
+  final String email;
+  final String username;
+  final String firstName;
+  final String lastName;
+  final DateTime dateOfBirth;
+  final String country;
+  final bool isStaff;
+  final bool isActive;
+
+  User({
+    required this.id,
+    required this.email,
+    required this.username,
+    required this.firstName,
+    required this.lastName,
+    required this.dateOfBirth,
+    required this.country,
+    required this.isStaff,
+    required this.isActive,
+  });
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+      id: json['id'],
+      email: json['email'],
+      username: json['username'],
+      firstName: json['first_name'],
+      lastName: json['last_name'],
+      dateOfBirth: DateTime.parse(json['date_of_birth']),
+      country: json['country'],
+      isStaff: json['is_staff'],
+      isActive: json['is_active'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'email': email,
+      'username': username,
+      'first_name': firstName,
+      'last_name': lastName,
+      'date_of_birth': dateOfBirth.toIso8601String(),
+      'country': country,
+      'is_staff': isStaff,
+      'is_active': isActive,
+    };
+  }
+}
