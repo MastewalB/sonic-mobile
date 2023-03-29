ab791db7778068a60b9c983bdb69b1a24733c6d5
Nahom Fufa <nahomn7@gmail.com>
2023-03-29 10:11:21 +0300

    folder structure for home

---
+++ lib/features/home/presentation/homepage.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import 'widgets/catchphrase.dart';
+import 'widgets/header.dart';
+import 'widgets/display_blocks.dart';
+
+class Homepage extends StatelessWidget {
+  const Homepage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
