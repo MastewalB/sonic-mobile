6341700f663a023d6a840cee5ca7b723f93d1b8b
MastewalB <se.mastewal.belete@gmail.com>
2023-04-23 09:54:16 +0300

    Create New Podcast

---
+++ lib/features/studio/bloc/create_podcast_bloc/create_podcast_state.dart
@@ -0,0 +1,40 @@
+part of 'create_podcast_bloc.dart';
+
+enum CreatePodcastStatus {
+  initial,
+  createPodcastLoading,
+  podcastCreated,
+  error
+}
+
+extension CreatePodcastStatusX on CreatePodcastStatus {
+  bool get isInitial => this == CreatePodcastStatus.initial;
+
+  bool get isCreatingPodcast =>
+      this == CreatePodcastStatus.createPodcastLoading;
+
+  bool get isPodcastCreated => this == CreatePodcastStatus.podcastCreated;
+
+  bool get isError => this == CreatePodcastStatus.error;
+}
+
+class CreatePodcastState extends Equatable {
+  final CreatePodcastStatus status;
+  final StudioPodcast? podcast;
+
+  const CreatePodcastState({
+    this.status = CreatePodcastStatus.initial,
+    this.podcast,
+  });
+
+  @override
+  List<Object?> get props => [];
+
+  CreatePodcastState copyWith(
+      {CreatePodcastStatus? status, StudioPodcast? podcast}) {
+    return CreatePodcastState(
+      status: status ?? this.status,
+      podcast: podcast ?? this.podcast,
+    );
+  }
+}
