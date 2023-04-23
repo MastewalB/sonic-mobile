9115663a4b13c5cafff92b38ffd398204162fd66
MastewalB <se.mastewal.belete@gmail.com>
2023-04-23 21:01:17 +0300

    Constant Items

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -1,1 +1,14 @@
-export 'media_query_manager.dart';
+import 'package:flutter/material.dart';
+
+class Constants{
+
+  //Characters
+  static const String kEllipsis = ' . . .';
+  static const String kLineSeparator = '\u2028';
+
+  //Icons
+  static const IconData settingsIcon = Icons.settings;
+  static const IconData drawerIcon = Icons.toc_rounded;
+  static const IconData searchIcon = Icons.search;
+
+}
