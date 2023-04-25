8cf6cce3fec1f3e9478657f159c96fc980f80960
MastewalB <se.mastewal.belete@gmail.com>
2023-04-25 15:50:13 +0300

    Routes - To Studio Library

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,5 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:sonic_mobile/dependency_provider.dart';
+import 'package:sonic_mobile/features/studio/bloc/studio_bloc/studio_bloc.dart';
+import 'package:sonic_mobile/features/studio/presentation/studio_library.dart';
+import 'package:sonic_mobile/features/studio/presentation/widgets/your_podcasts.dart';
 
 class PageRouter {
-  Route<dynamic>? generateRoute(RouteSettings routeSettings) {}
+  Route<dynamic>? generateRoute(RouteSettings routeSettings) {
+    switch (routeSettings.name) {
+      case StudioLibrary.routeName:
+        return MaterialPageRoute(builder: (context) {
+          return BlocProvider(
+            create: (context) => StudioBloc(
+                studioRepository: DependencyProvider.getHttpStudioRepository()!)
+              ..add(GetAllPodcastsByUserEvent(userId: "userId")),
+            child: StudioLibrary(),
+          );
+        });
+    }
+  }
 }
