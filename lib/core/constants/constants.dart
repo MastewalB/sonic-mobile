40fcb944c02e1c1c65577a950e323086511beb63
MastewalB <se.mastewal.belete@gmail.com>
2023-04-25 15:45:18 +0300

    Constants - Url endpoints, Error Messages

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 
-class Constants{
-
-  static const String apiUrl = "";
-  static const String refreshTokenUrl = "http://127.0.0.1:8000/api/v1/token/refresh/";
+class Constants {
+  static const String apiUrl = "http://127.0.0.1:8000/api/v1/";
+  static const String refreshTokenUrl = "${apiUrl}token/refresh/";
+  static const String accountsUrl = "${apiUrl}accounts/";
+  static const String followUrl = "${apiUrl}follow/";
+  static const String musicUrl = "${apiUrl}music/";
+  static const String searchUrl = "${apiUrl}search/";
+  static const String playlistUrl = "${apiUrl}playlist/";
+  static const String studioUrl = "${apiUrl}studio/";
+  static const String podcastsUrl = "${apiUrl}podcasts/";
 
   //Characters
   static const String kEllipsis = ' . . .';
@@ -14,4 +20,16 @@ class Constants{
   static const IconData drawerIcon = Icons.toc_rounded;
   static const IconData searchIcon = Icons.search;
 
+  //Error Messages
+  static const String notEnoughPermission = "You don't have enough permission.";
+  static const String loginRequired =
+      "You Need to Login to complete this action.";
+  static const String podcastCreated = "Podcast Created Successfully";
+  static const String connectionError = "A Network Problem Occurred";
+
+  //SnackBar Duration
+
+  static const Duration shortDuration = Duration(seconds: 3);
+  static const Duration normalDuration = Duration(seconds: 4);
+  static const Duration longDuration = Duration(seconds: 5);
 }
