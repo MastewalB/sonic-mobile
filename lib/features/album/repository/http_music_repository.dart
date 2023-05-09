993f674ab483b3cdde4b2621298d556e239c7f5c
Nahom Fufa <nahomn7@gmail.com>
2023-05-09 11:51:35 +0300

    music data provider update

---
+++ lib/features/album/repository/http_music_repository.dart
@@ -0,0 +1,82 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+// import 'package:sonic_mobile/models/album.dart';
+import 'package:sonic_mobile/models/models.dart';
+
+class AlbumDataProvider {
+  final _baseUrl = 'http://localhost:8000/api/v1/music/';
+  final http.Client httpClient;
+
+  AlbumDataProvider({required this.httpClient});
+
+  // get album by id
+  Future<Album> getAlbum(String albumId) async {
+    final response =
+        await httpClient.get(Uri.parse('$_baseUrl/albums/$albumId'));
+
+    if (response.statusCode == 200) {
+      final album = jsonDecode(response.body);
+      return Album.fromJson(album);
+    } else {
+      throw Exception('Failed to load album');
+    }
+  }
+
+  // get all albums
+  Future<Album> getAllAlbums() async {
+    final response = await httpClient.get(Uri.parse('$_baseUrl/albums/'));
+
+    if (response.statusCode == 200) {
+      final albums = jsonDecode(response.body);
+      return Album.fromJson(albums);
+    } else {
+      throw Exception('Failed to load album');
+    }
+  }
+
+  //get songs
+  Future<Song> getSongs() async {
+    final response = await httpClient.get(Uri.parse('$_baseUrl/songs/'));
+
+    if (response.statusCode == 200) {
+      final songs = jsonDecode(response.body);
+      return Song.fromJson(songs);
+    } else {
+      throw Exception('Failed to load songs');
+    }
+  }
+  // get albums by artist id
+}
+
+class ArtistDataProvider {
+  final _baseUrl = 'http://localhost:8000/api/v1/music/';
+  final http.Client httpClient;
+
+  ArtistDataProvider({required this.httpClient});
+
+  // get artist by id
+  Future<Artist> getArtist(String artistId) async {
+    final response =
+        await httpClient.get(Uri.parse('$_baseUrl/artists/$artistId'));
+
+    if (response.statusCode == 200) {
+      final artist = jsonDecode(response.body);
+      return Artist.fromJson(artist);
+    } else {
+      throw Exception('Failed to load artist');
+    }
+  }
+
+  // get all artists
+  Future<Artist> getAllArtists() async {
+    final response = await httpClient.get(Uri.parse('$_baseUrl/artists/'));
+
+    if (response.statusCode == 200) {
+      final artist = jsonDecode(response.body);
+      return Artist.fromJson(artist);
+    } else {
+      throw Exception('Failed to load aritst');
+    }
+  }
+}
