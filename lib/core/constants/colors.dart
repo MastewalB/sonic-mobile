5dd0519530dc2b28070a3d1da9ec25a7210b09fb
ludaze <luhanad@ymail.com>
2023-05-31 19:49:59 +0300

    added header and responsive file

--- lib/core/constants/colors.dart
+++ lib/core/constants/colors.dart
@@ -5,3 +5,9 @@ class SonicColors {
   static const Color shadowColor = Colors.black45;
   static const Color accentColor = Colors.white;
 }
+
+const primaryColor = Color(0xFF2697FF);
+const secondaryColor = Color(0xFF2A2D3E);
+const bgColor = Color(0xFF212332);
+
+const defaultPadding = 16.0;
