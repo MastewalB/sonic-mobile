b6d81be4dac57e1d35c0506d07190c4b5b3079d1
ludaze <luhanad@ymail.com>
2023-05-31 20:23:13 +0300

    playlist list component and header components added

---
+++ lib/screens/desktop_home.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:sonic_mobile/features/components/playlist_file.dart';
+import 'package:sonic_mobile/features/components/storage_details.dart';
+
+import '../../../constants.dart';
+import '../../components/header.dart';
+import '../../components/my_files.dart';
+import '../../components/recent_files.dart';
+import '../../components/playlist_header.dart';
+
+class DashboardScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: SingleChildScrollView(
+        primary: false,
+        padding: EdgeInsets.all(defaultPadding),
+        child: Column(
+          children: [
+            Header(),
+            PlaylistHeader(),
+            PlaylistFiles(),
+            
+            SizedBox(height: defaultPadding),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  flex: 5,
+                  child: Column(
+                    children: [
+
+                      MyFiles(),
+                      SizedBox(height: defaultPadding),
+                      RecentFiles(),
+                    ],
+                  ),
+                ),
+                SizedBox(width: defaultPadding),
+                // On Mobile we don't want to show it
+                Expanded(
+                  flex: 2,
+                  child: StorageDetails(),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
