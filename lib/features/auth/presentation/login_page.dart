f172b7660f4273dee58db3c88a74c9d9423e8b75
Nahom Fufa <nahomn7@gmail.com>
2023-06-07 22:47:19 +0300

    transition fix

--- lib/features/auth/presentation/login_page.dart
+++ lib/features/auth/presentation/login_page.dart
@@ -29,125 +29,95 @@ class _LoginPageState extends State<LoginPage> {
           WidgetsBinding.instance.addPostFrameCallback((_) {
             Navigator.pushReplacementNamed(context, LibraryPage.routeName);
           });
-        }
-
-        if (state.status.isLoading || state.status.isInitial) {
+        } else if (state.status.isLoading || state.status.isInitial) {
           return const Scaffold(
             body: Center(child: CircularProgressIndicator()),
           );
-        }
-        return Scaffold(
-          backgroundColor: Colors.black,
-          body: Container(
-            padding: const EdgeInsets.fromLTRB(100, 150, 100, 25),
-            child: Form(
-              child: SingleChildScrollView(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Text(
-                      "Login to Your Account",
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 25,
-                          fontWeight: FontWeight.w600),
-                    ),
-                    TextFormField(
-                      style: const TextStyle(
-                        color: Colors.white,
+        } else if (state.status.isLoaded || state.status.isError) {
+          return Scaffold(
+            backgroundColor: Colors.black,
+            body: Container(
+              padding: const EdgeInsets.fromLTRB(100, 150, 100, 25),
+              child: Form(
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        "Login to Your Account",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 25,
+                            fontWeight: FontWeight.w600),
                       ),
-                      controller: emailController,
-                      decoration: InputDecoration(
-                        border: UnderlineInputBorder(),
-                        labelText: 'Email',
-                        labelStyle: TextStyle(color: Colors.white),
-                        // hintText: 'Enter your email',
-                        icon: Icon(
-                          Icons.email,
+                      TextFormField(
+                        style: const TextStyle(
                           color: Colors.white,
                         ),
-                      ),
-                    ),
-                    TextFormField(
-                      controller: passwordController,
-                      style: TextStyle(color: Colors.white),
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        labelText: 'Password',
-                        labelStyle: TextStyle(color: Colors.white),
-                        // hintText: 'Enter your password',
-                        hintStyle: TextStyle(color: Colors.blue),
-                        icon: Icon(
-                          Icons.lock,
-                          color: Colors.white,
+                        controller: emailController,
+                        decoration: InputDecoration(
+                          border: UnderlineInputBorder(),
+                          labelText: 'Email',
+                          labelStyle: TextStyle(color: Colors.white),
+                          // hintText: 'Enter your email',
+                          icon: Icon(
+                            Icons.email,
+                            color: Colors.white,
+                          ),
                         ),
                       ),
-                    ),
-                    SizedBox(height: 20.0),
-                    ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                            shape: StadiumBorder(),
-                            foregroundColor: (Colors.white),
-                            backgroundColor: Colors.blue,
-                            minimumSize: const Size.fromHeight(50)),
-                        onPressed: () {
-                          if (emailController.text.isEmpty) {
-                            setState(() {
-                              emailValidator = false;
-                            });
-                          } else if (passwordController.text.isEmpty) {
-                            setState(() {
-                              passwordValidator = false;
-                            });
-                          }
-                          context.read<LoginBloc>().add(
-                                LoginSubmitEvent(
-                                  email: emailController.value.text,
-                                  password: passwordController.value.text,
-                                ),
-                              );
-                        },
-                        child: Padding(
-                          padding: EdgeInsets.all(15),
-                          child: Text('Log in'),
-                        )),
-                    Padding(
-                      padding: EdgeInsets.all(15),
-                      child: RichText(
-                        text: TextSpan(
-                          children: [
-                            TextSpan(
-                              text: 'Forgot your Password?',
-                              style: TextStyle(
-                                color: Colors.blue,
-                              ),
-                            ),
-                          ],
+                      TextFormField(
+                        controller: passwordController,
+                        style: TextStyle(color: Colors.white),
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          labelText: 'Password',
+                          labelStyle: TextStyle(color: Colors.white),
+                          // hintText: 'Enter your password',
+                          hintStyle: TextStyle(color: Colors.blue),
+                          icon: Icon(
+                            Icons.lock,
+                            color: Colors.white,
+                          ),
                         ),
                       ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.fromLTRB(0, 100, 0, 0),
-                      child: GestureDetector(
-                        onTap: () {
-                          WidgetsBinding.instance.addPostFrameCallback((_) {
-                            Navigator.pushReplacementNamed(
-                              context,
-                              SignUpPage.routeName,
-                            );
-                          });
-                        },
+                      SizedBox(height: 20.0),
+                      ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              shape: StadiumBorder(),
+                              foregroundColor: (Colors.white),
+                              backgroundColor: Colors.blue,
+                              minimumSize: const Size.fromHeight(50)),
+                          onPressed: () {
+                            if (emailController.text.isEmpty) {
+                              setState(() {
+                                emailValidator = false;
+                              });
+                            } else if (passwordController.text.isEmpty) {
+                              setState(() {
+                                passwordValidator = false;
+                              });
+                            }
+                            context.read<LoginBloc>().add(
+                                  LoginSubmitEvent(
+                                    email: emailController.value.text,
+                                    password: passwordController.value.text,
+                                  ),
+                                );
+                          },
+                          child: Padding(
+                            padding: EdgeInsets.all(15),
+                            child: Text('Log in'),
+                          )),
+                      Padding(
+                        padding: EdgeInsets.all(15),
                         child: RichText(
                           text: TextSpan(
                             children: [
                               TextSpan(
-                                  text: 'Dont have an account?  ',
-                                  style: TextStyle(color: Colors.white)),
-                              TextSpan(
-                                text: 'Sign up',
+                                text: 'Forgot your Password?',
                                 style: TextStyle(
-                                  fontSize: 15,
                                   color: Colors.blue,
                                 ),
                               ),
@@ -155,12 +125,44 @@ class _LoginPageState extends State<LoginPage> {
                           ),
                         ),
                       ),
-                    ),
-                  ],
+                      Padding(
+                        padding: EdgeInsets.fromLTRB(0, 100, 0, 0),
+                        child: GestureDetector(
+                          onTap: () {
+                            WidgetsBinding.instance.addPostFrameCallback((_) {
+                              Navigator.pushReplacementNamed(
+                                context,
+                                SignUpPage.routeName,
+                              );
+                            });
+                          },
+                          child: RichText(
+                            text: TextSpan(
+                              children: [
+                                TextSpan(
+                                    text: 'Dont have an account?  ',
+                                    style: TextStyle(color: Colors.white)),
+                                TextSpan(
+                                  text: 'Sign up',
+                                  style: TextStyle(
+                                    fontSize: 15,
+                                    color: Colors.blue,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
-          ),
+          );
+        }
+        return const Scaffold(
+          body: Center(child: CircularProgressIndicator()),
         );
       },
     );
